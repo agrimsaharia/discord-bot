6ac18135b5cc2bbb69f8c67db0c3b681e68080f7
Agrim Saharia <60686708+agrimsaharia@users.noreply.github.com>
2021-06-27 18:02:48 +0000

    added .gitignore removed vatsy insults from main.py

--- main.py
+++ main.py
@@ -3,16 +3,13 @@ import asyncio
 import discord
 import time
 import my_apis as api
-import random
 from alive import keep_alive
 
 intro_message = 'Discordwasiyo! Mai pappu hu\nseedhi baat kahu to mai bakwaas karne ke liye bana hu\nkuch commands se mai trigger hota hu\n\nMere se Hello sunke khudko meri tarah V.I.P samajhna chahte ho... \nto pehle mujhe "hello pappu" bolna padega\n\nMai dad jokes maar leta hu... roj mummy se seekhta hu\nJoke sunna hai to bolo --> "pappu joke suna"\n(waise mujhe pasand nahi agar tum mere jokes pe feedback na do to)\n\nKoi aapko attention dena pasand karta hai ya nahi(basically kaun kaun online hai) \njaanne ke liye dabaayein --> "koi hai?"\n\nLogo ki beizzati karna to pasand hai hi aapko, wo seva me bhi mai uplabdh hu...\nkisi chutiye ko insult karne ke liye --> "pappu insult" kehke mention kare\n\nmai apni mom ki bahut izzat karta hu, agar unpe "yo momma" waale joke maare to bahut pele jaoge\nhimmat hai to aajao!'
 
 intro_to_be_updated = '\n...\n...\n...\n...\n...\nAccha accha theek hai...\nWaise to mai bahut charming hu, but mujhe kabhi alvida kehna ho to "bye pappu" kardena\nafterall mujhe bhi to aaraam chahiye hota hai!\naur modi ki bakwaas se pak jao to mujhe --> "aaja pappu" kehke bula lena!'
 
-vatsalya = ['vatsy', 'vatsalya', 'batsy']
-vatsalya_insults = ['why do you have to mention a kidnapper in this \'safe for kids\' chat', 'vatsy...\tuhh!... \nhis name makes me puke everytime']
-feedback = ['chutiya --> 1', 'badiya --> 2']
+feedback = ['ghatiya --> 1', 'badiya --> 2']
 
 goodbyes_italian = ['Arrivederci!', 'Ciao! Ciao!', 'Accha chalta hu... voting me yaad rakhna']
 
@@ -91,11 +88,6 @@ async def on_message(message):
             await channel.send("I don't like to roast multiple people at once, give me only one mention!")
         else:
             await channel.send(api.get_evil_insult())
-
-    # insult vatsalya
-    elif any(word in msg for word in vatsalya):
-        insult = random.choice(vatsalya_insults)
-        await channel.send(insult)
 
     # # get rid of the bot
     # elif msg.startswith('bye pappu'):
